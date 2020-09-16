e5155a10c07fdf3d5b34b82654aec0bad4f82dd6
Suat <s.oezguer@gmail.com>
2020-09-16 19:30:15 +0100

    adding update script

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-TAG=`curl --silent "https://api.github.com/repos/$1/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
+TAG=`curl --silent "https://api.github.com/repos/sui77/scann3r/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
 git checkout -f $TAG
 npm update
