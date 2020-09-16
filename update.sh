1c25db810f158d7d5c2adaa98a5448bd304fc1ba
Suat Özgür <s.oezguer@gmail.com>
2020-09-17 00:07:20 +0200

    wip

--- update.sh
+++ update.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-TAG=`curl --silent "https://api.github.com/repos/sui77/scann3r/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
+
+ if [ "$#" -eq  "0" ]
+   then
+     echo "usage: ./update.sh [version|latest]"
+     exit
+ fi
+
+
+if [ "$1" = "latest" ]
+then
+  TAG=`curl --silent "https://api.github.com/repos/sui77/scann3r/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
+  echo Checkout latest release $TAG
+else
+  TAG=$1
+  echo Checkout $TAG
+fi
 git fetch
+
 git checkout -f $TAG
-npm update
+if [ $? -eq 0 ]; then
+    npm update
+    echo "Update done."
+else
+    echo "Update failed."
+fi
+
