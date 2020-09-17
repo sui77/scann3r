86af72a0e2f0a76559a6071b49fe8617808cde08
Suat Özgür <s.oezguer@gmail.com>
2020-09-17 02:04:36 +0200

    wip

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 sudo raspi-config nonint do_camera 0
 
 cd ~
-sudo apt update && apt install -y zip git nodejs npm pigpio redis-server imagemagick supervisor
+sudo apt update && sudo apt install -y zip git nodejs npm pigpio redis-server imagemagick supervisor
 
 git clone https://github.com/sui77/scann3r.git
 cd scann3r
